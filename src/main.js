0dcc09daedcdc599025107024af0fc4e4c17a034
Eros720 <ninessander720@gmail.com>
2021-08-19 21:22:29 -0700

    second commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 Vue.config.productionTip = false;
 
 new Vue({
-  render: (h) => h(App),
-}).$mount("#app");
+  el: "#app",
+  components: { custom: App },
+  template: `<div class="text-center">
+  <h1 class="bg-primary text-white p-3">
+  This is the main.js file
+  </h1>
+  <custom />
+  </div>`,
+});
